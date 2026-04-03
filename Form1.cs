4c9d195fdea428e2b3215104dd1412ce35b650d0
zenin5479 <zenin5479@gmail.com>
2026-04-03 09:57:52 +0300

    Update 0.5 Обновлен метод. Проведен рефакторинг

--- Form1.cs
+++ Form1.cs
@@ -21,8 +21,6 @@ namespace ExchangeNamedPipes
       {
          InitializeComponent();
 
-
-
          // Подключаем консоль к процессу
          AllocConsole();
          Console.WriteLine("=== Калькулятор факториала ===");
